0d6487b283104fc9e5eb7ece3953ce85fd7a7cc7
tianweizhang07 <tianwe@amazon.com>
2018-04-02 17:26:26 -0700

    change

--- dnn-mnist/v2/main.py
+++ dnn-mnist/v2/main.py
@@ -16,10 +16,11 @@ tf.logging.set_verbosity(tf.logging.INFO)
 MODEL_DIR = "./checkpoints/"
 DATA_DIR = "./data/"
 
-NUM_EPOCHS = 20000
+NUM_EPOCHS = 1000
 SAVE_EPOCHS = 1000
 LOG_EPOCHS = 100
 BATCH_SIZE = 256
+learning_rate = 1e-3
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', type=str, default='train',
@@ -29,23 +30,25 @@ parser.add_argument('--mode', type=str, default='train',
 def Train():
     x = tf.placeholder(tf.float32, [None, 28, 28, 1], name="X")
     y = tf.placeholder(tf.int32, [None, 10], name="Y")
+    keep_rate = tf.placeholder(tf.float32, name="keep_rate")
 
-    logits = network.CreateNet(x)
+    logits = network.CreateNet(x, keep_rate)
     soft = tf.nn.softmax(logits, name="soft")
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
                           logits=logits, labels=y), name="cost")
-    optimizer = tf.train.GradientDescentOptimizer(
-                learning_rate=0.001, name="optimizer").minimize(cost)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name="optimizer").minimize(cost)
     
     correct_pred = tf.equal(tf.argmax(soft, 1), tf.argmax(y, 1), name="correct_pred")
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name="accuracy")
 
-    mnist = dataset.read_data_sets(DATA_DIR, reshape=False, one_hot=True,
-                                   ratio=0.1, orig=8, target=9)
+    mnist = dataset.read_data_sets(DATA_DIR, reshape=False, one_hot=True)
+#    mnist = dataset.read_data_sets(DATA_DIR, reshape=False, one_hot=True,
+#                                   ratio=0.1, orig=1, target=2)
 
     with tf.Session() as sess:
         ckpt = tf.train.get_checkpoint_state(MODEL_DIR)
         if ckpt and ckpt.model_checkpoint_path:
+            sess.run(tf.global_variables_initializer())
             saver = tf.train.Saver(tf.all_variables())
             variables_to_restore = {}
             for v in tf.trainable_variables():
@@ -63,13 +66,13 @@ def Train():
 
         while step < global_step + NUM_EPOCHS:
             batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
-            feed_dict = {x: batch_x, y: batch_y}
+            feed_dict = {x: batch_x, y: batch_y, keep_rate: 0.8}
             cost_train, acc_train, _ = sess.run([cost, accuracy, optimizer], feed_dict=feed_dict)
 
             if step % LOG_EPOCHS == 0:
                 images_val = mnist.validation.images
                 labels_val = mnist.validation.labels
-                feed_dict = {x: images_val, y: labels_val}
+                feed_dict = {x: images_val, y: labels_val, keep_rate: 1.0}
                 _, acc_val = sess.run([cost, accuracy], feed_dict=feed_dict)
 
                 print('[%d]: loss = %.4f train_acc = %.4f validate_acc = %.4f'
@@ -88,9 +91,9 @@ def Train():
 
 
 def Eval():
-#    mnist = dataset.read_data_sets(DATA_DIR, reshape=False, one_hot=True)
-    mnist = dataset.read_data_sets(DATA_DIR, reshape=False, one_hot=True,
-                                   ratio=0.1, orig=8, target=9)
+    mnist = dataset.read_data_sets(DATA_DIR, reshape=False, one_hot=True)
+#    mnist = dataset.read_data_sets(DATA_DIR, reshape=False, one_hot=True,
+#                                   ratio=0.1, orig=1, target=1)
 
     g = tf.Graph()
  
@@ -103,11 +106,12 @@ def Eval():
         y = g.get_tensor_by_name("Y:0")
         cost = g.get_tensor_by_name("cost:0")
         accuracy = g.get_tensor_by_name("accuracy:0")
+        keep_rate = g.get_tensor_by_name("keep_rate:0")
 
         images_test = mnist.test.images
         labels_test = mnist.test.labels
 
-        feed_dict = {x: images_test, y: labels_test}
+        feed_dict = {x: images_test, y: labels_test, keep_rate: 1.0}
         _, acc_test = sess.run([cost, accuracy], feed_dict=feed_dict)
 
         print('test_acc = %.4f' % (acc_test))
